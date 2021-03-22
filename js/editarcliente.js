532359b2d181a133c55cb28e1117149edb16e8f9
keyner developers <keyner.caraballo@hotmail.com>
2021-03-22 15:14:25 -0500

    proyecto terminado

--- js/editarcliente.js
+++ js/editarcliente.js
@@ -1,6 +1,7 @@
 (function(){
 
     let DB;
+    let idCliente;
     //llenas los input
     const nombreInput = document.querySelector('#nombre');
     const emailInput = document.querySelector('#email');
@@ -17,7 +18,7 @@
         formulario.addEventListener('submit', actualizarCliente);
         //verificar la ID de la URL 
         const parametrosURL = new URLSearchParams(window.location.search);
-        const idCliente = parametrosURL.get('id');
+         idCliente = parametrosURL.get('id');
         if(idCliente){
             setTimeout( () =>{
                 obtenerCliente(idCliente);
@@ -26,6 +27,37 @@
         }
 
     });
+
+    function actualizarCliente(e){
+        e.preventDefault();
+        if(nombreInput.value ===''|| emailInput.value ===''|| empresaInput.value ==='' || telefonoInput.value ===''){
+              imprimirAlerta('Todos los campos son pbligatorios', 'error');
+            return;
+        }
+        //actualizar Cliente 
+        const clienteActualizado = {
+            nombre : nombreInput.value,
+            email : empresaInput.value,
+            empresa : emailInput.value,
+            telefono : telefonoInput.value,
+            id : Number(idCliente)
+
+        }
+        const transaction = DB.transaction(['crm'], 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+        objectStore.put(clienteActualizado);
+        transaction.oncomplete = function(){
+            imprimirAlerta('Editado Correctamente');
+            setTimeout(() =>{
+                window.location.href = 'index.html';
+            }, 3000)
+        };
+        transaction.onerror = function(){
+            imprimirAlerta('Hubo un error', 'error');
+        }
+
+
+    }
 
     function obtenerCliente(id){
         const transaction= DB.transaction(['crm'], 'readwrite');
